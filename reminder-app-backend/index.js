60bc6274a43848902c524193cd8000ee0ceff435
Apoorva <jainapoorva40@gmail.com>
2023-03-03 02:39:24 +0530

    first commit

---
+++ reminder-app-backend/index.js
@@ -0,0 +1,205 @@
+/*import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+require('dotenv').config()
+//for storing secret variables
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+
+//APP config
+const app=express()
+app.use(express.json()) //for using json
+app.use(express.urlencoded())  //for using api's
+app.use(cors())
+
+//DB config
+mongoose.connect("mongodb://127.0 0.1:27017/reminderAppDB", {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}, () =>{
+ console.log("DB connected")
+})
+
+const reminderSchema = new mongoose.Schema({
+    reminderMsg: String,
+    remindAt: String,
+    isReminded: Boolean
+})
+const Reminder = new mongoose.model("reminder", reminderSchema)
+//API routes
+
+app.get("/getAllReminder",(req,res)=>{
+    Reminder.find({},(err,reminderList)=>{
+        if(err){
+            console.log(err)
+        }
+        if(reminderList){
+            res.send(reminderList)
+        }
+    })
+
+})
+
+app.post("/addReminder",(req,res)=>{
+    const {reminderMsg,remindAt}=req.body
+    const reminder=new Reminder({
+        reminderMsg,
+        remindAt,
+        isReminded:false
+    })
+    reminder.save(err=>{
+        if(err){
+            console.log(err)
+        }
+        Reminder.find({},(err,reminderList)=>{
+            if(err){
+                console.log(err)
+            }
+            if(reminderList){
+                res.send(reminderList)
+            }
+        })
+    
+    })
+
+})
+app.post("/deleteReminder",(req,res)=>{
+    reminder.deleteOne({_id:req.body.id} ,()=>{
+        Reminder.find({},(err,reminderList)=>{
+            if(err){
+                console.log(err)
+            }
+            if(reminderList){
+                res.send(reminderList)
+            }
+        })
+    
+    })
+
+})
+/*
+app.get("/",(req,res)=>{
+    res.send("A message from BE")
+})
+app.listen(9000, () => {
+    console.log("Be started")
+})*/
+
+require('dotenv').config()
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+//APP config
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+//DB config
+mongoose.connect('mongodb://localhost:27017/reminderAppDB', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}, () => console.log("DB connected"))
+const reminderSchema = new mongoose.Schema({
+    reminderMsg: String,
+    remindAt: String,
+    isReminded: Boolean
+})
+const Reminder = new mongoose.model("reminder", reminderSchema)
+
+
+
+setInterval(() => {
+    Reminder.find({}, (err, reminderList) => {
+        if(err) {
+            console.log(err)
+        }
+        if(reminderList){
+            reminderList.forEach(reminder => {
+                if(!reminder.isReminded){
+                    const now = new Date()
+                    if((new Date(reminder.remindAt) - now) < 0) {
+                        Reminder.findByIdAndUpdate(reminder._id, {isReminded: true}, (err, remindObj)=>{
+                            if(err){
+                                console.log(err)
+                            }
+                            const accountSid = process.env.ACCOUNT_SID;
+
+                            const authToken = process.env.AUTH_TOKEN;
+                            const client = require('twilio')(accountSid, authToken); 
+ 
+                             client.messages 
+                             .create({ 
+                              body: reminder.reminderMsg, 
+                              from: 'whatsapp:+14155238886',       
+                              to: 'whatsapp:+919667350767' 
+                              }) 
+                              .then(message => console.log(message.sid)) 
+                              .done();
+                        })
+                    }
+                }
+            })
+        }
+    })
+},1000)
+;
+
+
+//Whatsapp reminding functionality
+
+
+
+//API routes
+app.get("/getAllReminder", (req, res) => {
+    Reminder.find({}, (err, reminderList) => {
+        if(err){
+            console.log(err)
+        }
+        if(reminderList){
+            res.send(reminderList)
+        }
+    })
+})
+app.post("/addReminder", (req, res) => {
+    const { reminderMsg, remindAt } = req.body
+    const reminder = new Reminder({
+        reminderMsg,
+        remindAt,
+        isReminded: false
+    })
+    reminder.save(err => {
+        if(err){
+            console.log(err)
+        }
+        Reminder.find({}, (err, reminderList) => {
+            if(err){
+                console.log(err)
+            }
+            if(reminderList){
+                res.send(reminderList)
+            }
+        })
+    })
+
+})
+app.post("/deleteReminder", (req, res) => {
+    Reminder.deleteOne({_id:req.body.id} ,()=>{
+        Reminder.find({},(err,reminderList)=>{
+            if(err){
+                console.log(err)
+            }
+            if(reminderList){
+                res.send(reminderList)
+            }
+        })
+    
+    })
+   
+})
+
+app.listen(9000, () => console.log("Be started"))
